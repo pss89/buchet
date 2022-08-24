d8628650d0cbb563ba627bb1778c35b44675ea47
박성식 <sosn3@naver.com>
2022-08-24 22:34:39 +0900

    77

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- 
+    - 각 컴포넌트 실제 호출?
+    - msg 변수는 각 컴포넌트에서 사용 가능
+   -->
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SubPage msg="두번째 단락임"/>
+</template>
+
+<script>
+// 컴포넌트 import
+import HelloWorld from './components/HelloWorld.vue'
+import SubPage from './components/SubPage.vue'
+
+export default {
+  name: 'App',
+  components: { // 로드 할 컴포넌트 작성
+    HelloWorld,SubPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
