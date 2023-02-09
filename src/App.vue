14f77236de7fc0c152a4132bccd9397cd94847c6
buchet89 <buchet@oa-world.com>
2023-02-09 12:11:25 +0900

    app vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <Header />
+  <Header msg="헤더임"/>
   <!-- 
     - 각 컴포넌트 실제 호출?
     - msg 변수는 각 컴포넌트에서 사용 가능
    -->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <!-- <SubPage msg="두번째 단락임"/> -->
+  <SubPage msg="두번째 단락임"/>
   <Footer />
 </template>
 
 <script>
 // 컴포넌트 import
+import Header from './components/common/Header.vue'
+import Footer from './components/common/Footer.vue'
 import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/common/Header.vue';
-import Footer from './components/common/Footer.vue';
-// import SubPage from './components/SubPage.vue'
+import SubPage from './components/SubPage.vue'
 
+// 컴포넌트 정의
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     Header,
-    Footer
+    Footer,
+    HelloWorld,
+    SubPage
 }
 }
 </script>
