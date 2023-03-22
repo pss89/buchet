2532b0cd067eed81160cf47c137f1938a9e5736d
박성식 <sosn3@naver.com>
2023-03-23 00:28:24 +0900

    헤더 반응형으로 만들기

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,20 +1,24 @@
 <template>
 	<nav class="navbar">
 		<div class="navbar__logo">
-            <font-awesome-icon icon="fa-brands fa-accusoft" />
-            <a href="">Buchet Intro</a>
+            <font-awesome-icon icon="fa-brands fa-accusoft" id="main_icon"/>
+            <a href="">Buchet Space</a>
         </div>
-		<ul class="navbar__menu">
+		<ul class="navbar__menu" v-bind:class="{active:isActive}">
             <li><a href="">Intro</a></li>
             <li><a href="">Tech Stack</a></li>
             <li><a href="">Reference</a></li>
             <li><a href="">Hobby</a></li>
         </ul>
-		<ul class="navbar__links">
-            <font-awesome-icon icon="fa-brands fa-twitter" />
-            <font-awesome-icon icon="fa-brands fa-facebook" />
-            <font-awesome-icon icon="fa-brands fa-instagram" />
+		<ul class="navbar__icons" v-bind:class="{active:isActive}">
+            <li><font-awesome-icon icon="fa-brands fa-twitter" /></li>
+            <li><font-awesome-icon icon="fa-brands fa-facebook" /></li>
+            <li><font-awesome-icon icon="fa-brands fa-instagram" /></li>
         </ul>
+
+        <a href="#" class="navbar__toggleBtn" v-on:click="toggle">
+            <font-awesome-icon icon="fa-solid fa-bars" />
+        </a>
 	</nav>
 </template>
 
@@ -23,6 +27,18 @@ export default {
     name: 'Header',
     props: {
         msg: String
+    },
+    // component 에서 사용 할 변수들 초기화
+    data:function(){
+        return{
+            isActive:false
+        }
+    },
+    // component 에서 사용 할 함수 초기화
+    methods:{
+        toggle: function(){ // 해당 함수를 클릭하면 isActive 변수를 변경
+            this.isActive=!this.isActive;
+        }
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,16 +5,16 @@ import { createMetaManager } from 'vue-meta' // vue meta load
 
 // fontawesome import
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fas,faBars } from '@fortawesome/free-solid-svg-icons'
 import { faAccusoft, faFacebook,faTwitter,faInstagram  } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(fas,faFacebook,faTwitter,faInstagram,faAccusoft )
+library.add(fas,faFacebook,faTwitter,faInstagram,faAccusoft,faBars )
 
 const app = createApp(App)
 
 // css 파일 로드
-import '@/assets/css/main.css'
 import '@/assets/css/reset.css'
+import '@/assets/css/main.css'
 
 // instance add
 app.use(router).use(createMetaManager()).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
