09760521fc3da9c2ec2661cdf3c0e9693ed8009a
박성식 <sosn3@naver.com>
2023-04-16 11:27:51 +0900

    path 변경

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     outputDir:'docs',
-    publicPath:'/buchet_vue',
+    publicPath:'/buchet',
     transpileDependencies: ['vue-meta'],
     lintOnSave:false
 })
