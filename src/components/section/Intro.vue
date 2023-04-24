9ee7595ee13bb66771098922363d4f8c47b53ff8
박성식 <sosn3@naver.com>
2023-04-25 00:07:30 +0900

    intro 진행 중

--- src/components/section/Intro.vue
+++ src/components/section/Intro.vue
@@ -1,6 +1,15 @@
 <template>
     <section id="intro">
-        <h1>{{msg}}</h1>
+        <!-- <h1>{{msg}}</h1> -->
+        <!-- <h1>제 이름은 박성식입니다.</h1> -->
+        <h1>Hello, I am</h1>
+        <h2>안녕하세요. 저는 박성식입니다.</h2>
+        <img :src="profile.profileUrl" alt="profileImg"/>
+        <!-- <ul class="nav-icons" v-bind:class="{active:isActive}">
+            <li @click="pageMove('github')"><font-awesome-icon icon="fa-brands fa-github" /></li>
+            <li @click="pageMove('blog')"><font-awesome-icon icon="fa-solid fa-blog" /></li>
+            <li @click="pageMove('instagram')"><font-awesome-icon icon="fa-brands fa-instagram" /></li>
+        </ul> -->
     </section>
 </template>
 
@@ -9,6 +18,11 @@ export default {
     name: 'Intro',
     props: {
         msg: String
+    },
+    data(){
+        return{
+            profile:{profileUrl:require("../../assets/img/main/pss_profile.jpeg")}
+        }
     }
 }
 </script>
@@ -22,7 +36,7 @@ header ul.menu li{float:left; padding:10px 20px; list-style:none;}
 
 a{text-decoration:none; color:#333;}
 
-#intro{
+/* #intro{
     width:100%;
     height:100%;
     margin:0 auto;
@@ -30,5 +44,10 @@ a{text-decoration:none; color:#333;}
     background-repeat: no-repeat;
     background-position: center center;
     background-size: 100% 100%;
+} */
+#intro{
+    width:100%;
+    height:50%;
+    margin:0 auto;
 }
 </style>
