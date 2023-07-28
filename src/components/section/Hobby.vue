6cf78600856b3c93d54f4350117e37ee9ded1df2
포털개발_박성식 <sspark@thebiz.kr>
2023-07-28 13:43:40 +0900

    취미 components 추가

--- src/components/section/Hobby.vue
+++ src/components/section/Hobby.vue
@@ -1,15 +1,37 @@
-<!-- 취미 -->
 <template>
-  <div>
-    취미
+  <div class="flex flex-wrap justify-center">
+    <h1>취미</h1>
+    <!-- 반복문을 활용하여 각 취미 카드를 표시합니다. -->
+    <div v-for="hobby in hobbies" :key="hobby.id" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 py-4">
+      <div class="bg-white rounded-lg shadow-md p-4">
+        <!-- <img :src="hobby.image" alt="Hobby Image" class="h-32 w-full object-cover rounded-lg"> -->
+        <h3 class="text-xl font-semibold mt-2">{{ hobby.title }}</h3>
+        <p class="text-gray-600 mt-2">{{ hobby.description }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Hobby'
-}
+  data() {
+    return {
+      hobbies: [
+        {
+          id: 1,
+          title: "Photography",
+          description: "I love capturing beautiful moments with my camera.",
+          image: "/src/assets/hobbies/photography.jpg",
+        },
+        {
+          id: 2,
+          title: "Cooking",
+          description: "Cooking is my passion. I enjoy experimenting with new recipes.",
+          image: "/src/assets/hobbies/cooking.jpg",
+        },
+        // Add more hobbies here...
+      ],
+    };
+  },
+};
 </script>
-
-<style scoped>
-</style>
--- src/components/section/Project.vue
+++ src/components/section/Project.vue
@@ -1,53 +1,38 @@
 <template>
-  <v-card
-    elevation="24"
-    max-width="444"
-    class="mx-auto"
+  <h1>프로젝트</h1>
+  <v-carousel
+    height="400"
+    hide-delimiters
+    progress="primary"
   >
-    <v-carousel
-      :continuous="false"
-      :show-arrows="false"
-      hide-delimiter-background
-      delimiter-icon="mdi-square"
-      height="300"
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
     >
-      <v-carousel-item
-        v-for="(slide, i) in slides"
-        :key="i"
+      <v-sheet
+        height="100%"
       >
-        <v-sheet
-          :color="colors[i]"
-          height="100%"
-          tile
-        >
-          <div class="d-flex fill-height justify-center align-center">
-            <div class="text-h2">
-              {{ slide }} Slide
-            </div>
+        <div class="d-flex fill-height justify-center align-center">
+          <div class="text-h2">
+            {{ slide }} Slide
           </div>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
-  </v-card>
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
   export default {
     data () {
       return {
-        colors: [
-          'green',
-          'secondary',
-          'yellow darken-4',
-          'red lighten-2',
-          'orange darken-1',
-        ],
         slides: [
           'First',
           'Second',
           'Third',
           'Fourth',
           'Fifth',
+          'Sixth',
         ],
       }
     },
