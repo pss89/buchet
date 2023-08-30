83c847fdb48f4e8f21413c29eb80270f64322a6b
포털개발_박성식 <sspark@thebiz.kr>
2023-08-30 16:13:52 +0900

    footer top,bottom 아이콘 수정

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,33 +1,48 @@
 <template>
-    <footer class="bg-cyan-600 text-white py-8 w-full bottom-0 left-0 mt-20">
-        <div class="container mx-auto flex flex-col items-center">
-            <div class="text-center md:text-left mb-4 md:mb-0">
-                <h3 class="text-2xl font-bold">Buchet Introduction WebSite</h3>
-                <p class="text-sm">© 2023 All rights reserved.</p>
-            </div>
-        </div>
-    </footer>
+<footer class="bg-cyan-600 text-white py-8 w-full bottom-0 left-0 mt-20">
+  <div class="container mx-auto flex flex-col items-center">
+    <div class="text-center md:text-left mb-4 md:mb-0">
+      <h3 class="text-2xl font-bold">Buchet Introduction WebSite</h3>
+      <p class="text-sm">© 2023 All rights reserved.</p>
+    </div>
+  </div>
+</footer>
 
-    <button
-        id="scrollToTopBtn"
-        class="fixed bottom-4 right-4 bg-cyan-600 hover:bg-cyan-700 text-white py-2 px-4 rounded-full shadow-lg"
-        @click="scrollToTop"
-    >
-        TOP
-    </button>
+<div>
+  <!-- 스크롤 시 TOP 버튼 보이기 -->
+  <button
+    class="fixed bottom-16 right-4 bg-cyan-600 hover:bg-cyan-700 text-white py-2 px-4 rounded-full shadow-lg"
+    @click="scrollToTop('top')"
+  >
+  <!-- TOP -->
+  <font-awesome-icon :icon="['fas', 'arrow-up']" />
+  </button>
+
+  <!-- 스크롤 시 BOTTOM 버튼 보이기 -->
+  <button
+    class="fixed bottom-4 right-4 bg-cyan-600 hover:bg-cyan-700 text-white py-2 px-4 rounded-full shadow-lg"
+    @click="scrollToTop('bottom')"
+  >
+  <font-awesome-icon :icon="['fas', 'arrow-down']" />
+  </button>
+</div>
 </template>
 
 <script>
 export default {
-    name: 'Footer',
-    props: {
-        msg: String
-    },
-    methods: {
-        scrollToTop() {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-        }
+  name: 'Footer',
+  props: {
+    msg: String
+  },
+  methods: {
+    scrollToTop(type) {
+      if (type == 'top') {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      } else {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+      }
     }
+  }
 }
 </script>
 
