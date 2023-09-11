28fe50fa77d5537479c1a2c5b8474e732d70f675
buchet <sosn3@naver.com>
2023-09-11 21:12:10 +0900

    hobby

--- src/components/sections/Hobby.vue
+++ src/components/sections/Hobby.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
-    <div v-for="hobby in constants.HOBBY.list" :key="hobby.key" class="bg-white p-4 shadow-md">
-      <img v-lazy="hobby.img" :alt="hobby.title" class="w-full h-auto mb-4">
-      <h3 class="text-xl font-semibold mt-2">{{ hobby.title }}</h3>
-      <p class="text-gray-800" v-html="hobby.description"></p>
+  <div class="container mx-auto p-4 mt-10" id="hobby">
+    <h1 class="text-4xl font-bold mb-6">취미</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+      <div v-for="hobby in constants.HOBBY.list" :key="hobby.key" class="bg-white p-4 shadow-md">
+        <img v-lazy="hobby.img" :alt="hobby.title" class="w-full h-auto mb-4">
+        <h3 class="text-xl font-semibold mt-2">{{ hobby.title }}</h3>
+        <p class="text-gray-800" v-html="hobby.description"></p>
+      </div>
     </div>
   </div>
 </template>
