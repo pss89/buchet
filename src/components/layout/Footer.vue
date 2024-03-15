2961a72a938bf280db02a0715b734191228edc5a
klinelex_sspark <sspark@klinelex.kr>
2024-03-15 09:01:16 +0900

    수정 배포

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -5,23 +5,14 @@
         <h3 class="text-2xl font-bold">Buchet Introduction WebSite</h3>
         <p class="text-sm">Vue Version : {{ $vueVersion }}</p>
         <p class="text-sm">Device Info : {{ $isMobile ? 'Mobile' : 'PC' }}</p>
-        <!-- <p class="text-sm">System Mode : {{ $isDarkMode ? '다크모드 On' : '다크모드 Off' }}</p> -->
+        <p class="text-sm">System Mode : {{ $isDarkMode ? 'DarkMode On' : 'DarkMode Off' }}</p>
         <p class="text-sm">© 2023 All rights reserved.</p>
       </div>
     </div>
   </footer>
 
   <div>
-    <!-- 스크롤 시 TOP 버튼 보이기 -->
-    <!-- <button
-      class="fixed bottom-16 right-4 bg-slate-100 dark:bg-slate-800 hover:bg-slate-700 py-2 px-4 rounded-full shadow-lg"
-      @click="scrollMoving('top')"
-    > -->
-      <!-- TOP -->
-      <!-- <font-awesome-icon :icon="['fas', 'arrow-up']" />
-    </button> -->
-
-    <!-- 스크롤 시 BOTTOM 버튼 보이기 -->
+    <!-- 스크롤 시 top, bottom 버튼 보이기 -->
     <button
       class="fixed bottom-4 right-4 bg-slate-100 dark:bg-slate-800 hover:bg-slate-700 py-2 px-4 rounded-full shadow-lg"
       @click="scrollMoving()"
