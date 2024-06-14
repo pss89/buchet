26dbcbd1423949534b79e370b8bad2e094c8d9d6
sspark89 <njcjjhx5bj@privaterelay.appleid.com>
2024-06-15 00:58:43 +0900

    123

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'docs',
-    publicPath: '/buchet/',
+    publicPath: '/',
     rollupOptions: {
       // input: '/buchet'
       input: 'src/main.js'
